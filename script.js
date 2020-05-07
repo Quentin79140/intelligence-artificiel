08a92ed1c4596c28e09aa1922c565237d3645823
Quentin79140 <60688891+Quentin79140@users.noreply.github.com>
2020-05-07 11:56:44 +0200

    init

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ async function start() {
 }
 
 function loadLabeledImages() {
-  const labels = ['Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark', 'Dr Bruce Banner']
+  const labels = ['Black Widow', 'Captain America', 'Captain Marvel','Dr Bruce Banner', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark']
   return Promise.all(
     labels.map(async label => {
       const descriptions = []
